572328837ec0bb55283ef8815dcd5f323af33132
Cedric Nicoloso <cnicoloso@octo.com>
2018-03-24 22:01:35 +1100

    Display 'Server listening' on listen() callback

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require('./src/app')
 
 const port = process.env.PORT || 3000
-app.listen(port)
-console.log(`Server listening on ${port}...`)
+app.listen(port, () => {
+  console.log(`Server listening on ${port}...`)
+})
